50f94648e3de932e4c91d5ed5f485a7f13196ffa
snow1s-me <Vanzzhenya604@gmail.com>
2025-05-10 11:27:55 +0300

    ozinka

---
+++ 7.py
@@ -0,0 +1,9 @@
+balu = int(input("Введіть кількість балів: "))
+if 0 <= balu <= 49:
+    print("Незадовільно")
+elif 50 <= balu <= 69:
+    print("Задовільно")
+elif 70 <= balu <= 89:
+    print("Добре")
+elif 90 <= balu <= 100:
+    print("Відмінно")
